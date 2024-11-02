ae4f910013c21c0f49c31a20230910175ac404a2
Lucas52352 <lucasrius86@gmail.com>
2024-11-02 15:54:42 -0300

    Authentication Repository

---
+++ CineAPI/CineRepository/Repositories/Contracts/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using CineRepository.Models.Entities;
+
+namespace CineRepository.Repositories.Contracts
+  {
+  public interface IUserRepository
+    {
+    Task RegisterAsync(UserAccount newUser);
+    Task<UserAccount> GetUserByNameAsync(string username);
+    Task<IEnumerable<UserAccount>> GetAllUsersAsync();
+
+    }
+  }
---
+++ CineAPI/CineRepository/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using CineRepository.Models.Entities;
+using CineRepository.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace CineRepository.Repositories.Implementations
+  {
+  public class UserRepository : IUserRepository
+    {
+
+    private readonly Cine_1W3_TPContext _context;
+
+    public UserRepository(Cine_1W3_TPContext context)
+      {
+      _context = context;
+      }
+
+    public async Task<IEnumerable<UserAccount>> GetAllUsersAsync()
+      {
+      return await _context.UserAccounts.ToListAsync();
+      }
+
+    public async Task<UserAccount> GetUserByNameAsync(string username)
+      {
+      return await _context.UserAccounts
+        .Include(u => u.Customer)
+        .FirstOrDefaultAsync(u => u.Username == username);
+      }
+
+    public async Task RegisterAsync(UserAccount newUser)
+      {
+      await _context.UserAccounts.AddAsync(newUser);
+      await _context.SaveChangesAsync();
+      }
+    }
+  }
