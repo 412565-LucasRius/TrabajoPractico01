2dca7bfac0ae11fbb4e956ac842f47dae21e60ab
412241CristianCaporusso <412241@tecnicatura.frc.utn.edu.ar>
2024-11-02 11:27:29 -0300

    NuevaAPI

---
+++ TPCineApi/CineAPI/CineAPI/Controllers/MoviesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+
+
+    }
+}
