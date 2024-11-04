5e7674d53c57919cd709b958abcc52751debc6b7
Lucas52352 <lucasrius86@gmail.com>
2024-11-03 21:18:35 -0300

    Booking Repository started

---
+++ CineAPI/CineRepository/Repositories/Implementations/BookingRepository.cs
@@ -0,0 +1,28 @@
+﻿using CineRepository.Models.Entities;
+using CineRepository.Repositories.Contracts;
+
+namespace CineRepository.Repositories.Implementations
+  {
+  public class BookingRepository : IBookingRepository
+    {
+    public Task<Booking> GetBookingById(int id)
+      {
+      throw new NotImplementedException();
+      }
+
+    public Task<IEnumerable<Booking>> GetBookingsByUser(string userId)
+      {
+      throw new NotImplementedException();
+      }
+
+    public Task<bool> SaveBooking(Booking booking)
+      {
+      throw new NotImplementedException();
+      }
+
+    public Task<Booking> UpdateBookingState(Booking booking)
+      {
+      throw new NotImplementedException();
+      }
+    }
+  }
