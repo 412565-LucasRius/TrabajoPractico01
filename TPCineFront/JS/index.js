544ed574707c06fe88cd67eb14e9792010b85f21
412241CristianCaporusso <412241@tecnicatura.frc.utn.edu.ar>
2024-11-07 16:58:57 -0300

    se muestran imagenes en el carrousel

--- TPCineFront/JS/index.js
+++ TPCineFront/JS/index.js
@@ -32,29 +32,28 @@ async function fetchMovies() {
 // Cargar las películas en el carrusel
 function loadMovies(movies) {
     const movieGrid = document.getElementById('premiere-list');
+
+    // Agrupar las películas en conjuntos de 4 para cada elemento del carrusel
     for (let i = 0; i < movies.length; i += 4) {
         const carouselItem = document.createElement('div');
         carouselItem.className = `carousel-item ${i === 0 ? 'active' : ''} movie-card-group`;
 
+        // Agregar 4 películas por elemento del carrusel
         for (let j = i; j < i + 4 && j < movies.length; j++) {
             const movie = movies[j];
             const movieCard = document.createElement('div');
             movieCard.className = 'col-md-3 movie-card';
             movieCard.onclick = () => openModal(movie.title, movie.description, movie.movieId);
 
-            const releaseDate = new Date(movie.releaseDate).toLocaleDateString('es-ES', {
-                day: '2-digit',
-                month: '2-digit',
-                year: 'numeric'
-            });
 
             movieCard.innerHTML = `
                 <div class="card card-body movie-poster">
-                    <img class="img-fluid" src="${movie.posterUrl}" alt="${movie.title}">
-                    <div class="movie-info">
-                        <span class="release-date">Disponible hasta: ${releaseDate}</span>
+                    <img class="img-fluid" src="assets/${movie.imageName}.jfif" alt="${movie.title}">
+                   <div class="movie-info">
+                        <span class="duration" style="color: white; background-color: red; padding: 5px; border-radius: 5px;">Duración: ${movie.duration} minutos</span>
                     </div>
                 </div>
+                <div class="movie-details">
                 <h3>${movie.title}</h3>
                 <a href="asientos.html?movieId=${movie.movieId}" class="btn btn-primary">Seleccionar Asientos</a>
             `;
@@ -68,7 +67,6 @@ function loadMovies(movies) {
 
 document.addEventListener('DOMContentLoaded', fetchMovies);
 
-// Cargar los cines
 async function fetchCinemas() {
     try {
         const response = await fetch('https://localhost:7276/api/Cinema/GetAllCinemas');
@@ -81,8 +79,10 @@ async function fetchCinemas() {
 
 function loadCinemas(cinemas) {
     const select = document.getElementById("locationSelect");
+    // Limpiar cualquier opción existente antes de agregar las nuevas
     select.innerHTML = '';
 
+    // Agregar la opción predeterminada
     const opcionPredeterminada = document.createElement('option');
     opcionPredeterminada.value = '';
     opcionPredeterminada.disabled = true;
@@ -90,6 +90,7 @@ function loadCinemas(cinemas) {
     opcionPredeterminada.textContent = 'Seleccionar Cine';
     select.appendChild(opcionPredeterminada);
 
+    // Agregar las opciones del arreglo
     cinemas.forEach(cinema => {
         const option = document.createElement('option');
         option.value = cinema.cinemaId;
